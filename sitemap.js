bb693227a521ef2f8c523c1d9dac81a0f71b3730
Steven Barquet <mlmjm123@gmail.com>
2021-12-04 10:42:08 -0600

    readme modified

---
+++ sitemap.js
@@ -0,0 +1,15 @@
+// ---Dependencys
+const path = require('path');
+const buildSitemap = require('react-sitemap-stevedevops');
+
+// ---Config
+const currentPath = path.resolve(__dirname);
+const siteDomain = 'https://www.forgemytech.com'
+const aditionalWebsites = [
+  'https://web.whatsapp.com/',
+  'https://babel.dev/docs/en/babel-parser',
+  'https://www.tutorialkart.com/nodejs/create-file-in-nodejs-using-node-fs-module/'
+]
+
+// ---Script
+buildSitemap(`${currentPath}/RoutesExample1.tsx`, currentPath, siteDomain, aditionalWebsites);
